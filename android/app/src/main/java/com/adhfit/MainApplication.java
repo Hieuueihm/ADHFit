519a76b9548cd1115481ac4dbdcc85060ea16323
Hieu <gbmhieu123@gmail.com>
2023-09-23 10:47:30 +0700

    login

--- android/app/src/main/java/com/adhfit/MainApplication.java
+++ android/app/src/main/java/com/adhfit/MainApplication.java
@@ -9,7 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
-import com.oblador.vectoricons.VectorIconsPackage;
+// import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -20,15 +20,13 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-    @Override
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for
       // example:
       // packages.add(new MyReactNativePackage());
-      packages.add(new SharedStoragePackager());
-      packages.add(new VectorIconsPackage());
+      // packages.add(new VectorIconsPackage());
       return packages;
     }
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,25 @@
-import React from "react";
-import { Text, View, Image, SafeAreaView, StyleSheet, TextInput, TouchableOpacity, InputField } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, SafeAreaView, StyleSheet, TextInput, TouchableOpacity, InputField, Alert } from "react-native";
 import { COLORS, APP_NAME } from '../../constants/index.js';
-import Icon from 'react-native-vector-icons/dist/Ionicons.js';
-
+// import Icon from '../../android/app/src/main/assets/fonts/FontAwesome.ttf'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons.js';
 
 const Login = () => {
+    const [isTextInputEmailFocused, setIsTextInputEmailFocused] = useState(false);
+    const [isTextInputCaptchaFocused, setIsTextInputCaptchaFocused] = useState(false);
+    const [email, setEmail] = useState('');
+    const validateEmail = (email) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+
+    };
+    const handleGetCaptcha = () => {
+        if (!validateEmail(email)) {
+            Alert.alert('Lỗi email', 'Vui lòng nhập đúng đinh dạng email');
+            return;
+        }
+
+    };
     return (
         <SafeAreaView
             style={styles.container}
@@ -18,34 +33,223 @@ const Login = () => {
 
             <View
                 style={{
-                    flexDirection: 'row',
+                    flex: 1,
                     justifyContent: 'center',
-                    padding: 20,
+                    alignItems: 'center',
+                    maxHeight: 80
+
                 }}
             >
                 <Text
-                    style={styles.greenText}
+                    style={{
+                        marginBottom: 4,
+                        color: COLORS.green,
+                        fontSize: 24,
+                        fontWeight: 500
+                    }}
                 >
                     {APP_NAME.name}
+                </Text>
+
+                <Text
+                    style={{
+                        color: COLORS.grey1
+                    }}
+                >
+                    Please login to your account
                 </Text>
             </View>
 
             <View
                 style={{
                     marginTop: 10,
-                    backgroundColor: 'red',
-                    paddingHorizontal: 25
-
+                    paddingHorizontal: 32,
+                    paddingEnd: 32
                 }}
             >
-                <View>
-                    <Icon name="arrow-back" size={30} color="#000" />
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        borderBottomColor: isTextInputEmailFocused ? COLORS.green1 : COLORS.grey,
+                        borderBottomWidth: 2,
+                        marginBottom: 25,
+                    }}
+                >
+                    <MaterialCommunityIcons
+                        name='email-outline'
+                        size={32}
+                        color={isTextInputEmailFocused ? COLORS.green : COLORS.grey}
+                        style={{
+                            marginTop: 4,
+                        }}
+                    ></MaterialCommunityIcons>
+                    <TextInput
+                        placeholder="Email address"
+                        onFocus={() => setIsTextInputEmailFocused(true)}
+                        onBlur={() => setIsTextInputEmailFocused(false)}
+                        onChangeText={text => setEmail(text)}
+                        style={{
+                            flex: 1,
+                            paddingVertical: 0,
+                            color: COLORS.black,
+                            paddingLeft: 5
+
+                        }}
+                        placeholderTextColor={COLORS.grey}
+                        keyboardType="email-address"
+
+                    />
                 </View>
 
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        borderBottomColor: isTextInputCaptchaFocused ? COLORS.green1 : COLORS.grey,
+                        borderBottomWidth: 2,
+                        marginBottom: 25,
+
+                    }}
+                >
+                    <MaterialCommunityIcons
+                        name='lock-outline'
+                        size={32}
+                        color={isTextInputCaptchaFocused ? COLORS.green : COLORS.grey}
+                        style={{
+                            marginTop: 4,
+                        }}
+                    ></MaterialCommunityIcons>
+                    <TextInput
+
+                        placeholder="Captcha"
+                        onFocus={() => setIsTextInputCaptchaFocused(true)}
+                        onBlur={() => setIsTextInputCaptchaFocused(false)}
+                        style={{
+                            flex: 1,
+                            paddingVertical: 0,
+                            paddingLeft: 5,
+                            color: COLORS.black
+                        }}
+                        placeholderTextColor={COLORS.grey}
+
+                        keyboardType="number-pad"
+
+                    />
+                </View>
+
+                <TouchableOpacity
+                    onPress={handleGetCaptcha}
+                    style={{
+                        marginTop: 10,
+                        backgroundColor: COLORS.captcha,
+                        padding: 10,
+                        borderRadius: 30,
+                        marginBottom: 27,
+                        marginEnd: 46,
+                        marginStart: 46,
+                        borderColor: COLORS.black,
+                        borderWidth: 1
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: COLORS.black,
+                            textTransform: "uppercase",
+                            textAlign: "center",
+                            fontSize: 18,
+                            fontWeight: "400"
+
+
+                        }}
+                    >
+                        get Captcha
+                    </Text>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: COLORS.signin,
+                        padding: 10,
+                        borderRadius: 30,
+                        marginBottom: 10,
+                        marginEnd: 80,
+                        marginStart: 80,
+
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: COLORS.black,
+                            textTransform: "uppercase",
+                            textAlign: "center",
+                            fontSize: 18,
+                            fontWeight: "400"
+
+
+                        }}
+                    >
+                        sign in
+                    </Text>
+
+                </TouchableOpacity>
+
+                <Text
+                    style={{
+                        textAlign: 'center',
+                        marginTop: 10,
+                        marginLeft: 10,
+                        color: COLORS.black29
+                    }}
+                >Email did not receive verification code?</Text>
+
+                <View style={{
+                    alignItems: 'center',
+                    marginTop: 20
+                }}>
+                    <Text
+                        style={{
+                            paddingLeft: 10,
+                            paddingRight: 10,
+                            marginTop: 10,
+                            backgroundColor: 'white',
+                            position: 'relative',
+                            zIndex: 1
+                        }}
+                    >
+                        OR SIGN IN WITH
+                    </Text>
+                    <View
+                        style={{
+                            borderBottomWidth: 1,
+                            borderBottomColor: COLORS.green,
+                            width: '80%',
+                            position: 'absolute',
+                            top: '70%',
+                            zIndex: 0
+                        }}
+                    />
+                </View>
+
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-around', marginTop: 40
+                }}>
+                    <TouchableOpacity
+                    >
+                        <Image source={require('../assets/logos/facebook.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/logos/google.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/logos/github.png')} />
+                    </TouchableOpacity>
+                </View>
             </View>
 
 
-        </SafeAreaView>
+
+        </SafeAreaView >
     )
 }
 const styles = StyleSheet.create({
@@ -56,7 +260,7 @@ const styles = StyleSheet.create({
     },
     mainLogo: {
         height: 100,
-        width: 100,
+        width: 120,
         marginTop: 40
 
     },
@@ -64,13 +268,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-end',
         minHeight: 100,
-    },
-    greenText: {
-        color: COLORS.green,
-        fontSize: 20,
-        lineHeight: 25.3,
-
-
     }
 });
 
