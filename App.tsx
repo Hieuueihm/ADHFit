a57604ef6808d775f835aaa622a6ed692a707941
Hieu <gbmhieu123@gmail.com>
2023-10-01 11:29:50 +0700

    complete weather task

--- App.tsx
+++ App.tsx
@@ -3,16 +3,19 @@ import Login from './src/screens/welcome/Login';
 const Stack = createNativeStackNavigator();
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/screens/home/Home';
+import WeatherScreen from './src/screens/home/WeatherScreen';
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login}
+        {/* <Stack.Screen name="Login" component={Login}
           options={{ headerShown: false }}
         />
         <Stack.Screen name="Home" component={Home}
           options={{ headerShown: false }}
+        /> */}
+        <Stack.Screen name="Weather" component={WeatherScreen} options={{ headerShown: false }}
         />
         {/* <Stack.Screen name="Notifications" component={Notifications} />
       <Stack.Screen name="Profile" component={Profile} />
--- constants/index.js
+++ constants/index.js
@@ -5,3 +5,4 @@ export {
     COLORS,
     APP_NAME
 }
+export const weatherAPIKey = 'baccaf660dad4488971151859232909';
