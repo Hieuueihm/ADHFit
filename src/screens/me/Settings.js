57fb0d2f2fda857667850f5a36acfbb8adbaeeb5
Vũ Tuấn Anh <92041804+VuTuanAnh-1368@users.noreply.github.com>
2023-10-17 09:17:44 +0700

    Update Settings.js
    Update (Conflict edit)

--- src/screens/me/Settings.js
+++ src/screens/me/Settings.js
@@ -3,9 +3,6 @@ import { View, Text, SafeAreaView, TextInput, Image, ImageBackground, Button, To
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { COLORS } from "../../../constants";
 
-<<<<<<< HEAD
-export default function Settings () {
-=======
 export default function Setting () {
 >>>>>>> 5c9a7159903bd3fe3e2af696f4cd06ffe1e6ce7e
     //Change Language
